24a3da9d4d0236b9ab554cb3277052fbec92196b
Usuario <Usuario@10.22.148.100>
2019-10-30 10:19:45 +0000

    Encontramos el error anterior, la expresion regular Modicamos la expresion regular apra hacerla mas precisa

--- src/kata4_model/Mail.java
+++ src/kata4_model/Mail.java
@@ -13,6 +13,7 @@ public class Mail {
     
     public static boolean isMail(String lineFile){
         
-        return lineFile.matches("[-\\w\\.]+@\\w\\.\\w+");
+        //return lineFile.matches("[-\\w\\.]+@\\w+\\.\\w+");
+        return lineFile.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
     }
 }
